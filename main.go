7700444f83aff2cc535ad4e202712f6fe00a2bf9
Przemek Delewski <pdelewski@sumologic.com>
2023-07-13 16:48:36 +0200

    cleaning

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"go/ast"
-	"go/importer"
 	"go/parser"
 	"go/token"
 	"go/types"
@@ -37,57 +36,6 @@ func GetCommandName(args []string) string {
 	return cmd
 }
 
-// GetMostInnerAstIdent takes most inner identifier used for
-// function call. For a.b.foo(), `b` will be the most inner identifier.
-func GetMostInnerAstIdent(inSel *ast.SelectorExpr) *ast.Ident {
-	var l []*ast.Ident
-	var e ast.Expr
-	e = inSel
-	for e != nil {
-		if _, ok := e.(*ast.Ident); ok {
-			l = append(l, e.(*ast.Ident))
-			break
-		} else if _, ok := e.(*ast.SelectorExpr); ok {
-			l = append(l, e.(*ast.SelectorExpr).Sel)
-			e = e.(*ast.SelectorExpr).X
-		} else if _, ok := e.(*ast.CallExpr); ok {
-			e = e.(*ast.CallExpr).Fun
-		} else if _, ok := e.(*ast.IndexExpr); ok {
-			e = e.(*ast.IndexExpr).X
-		} else if _, ok := e.(*ast.UnaryExpr); ok {
-			e = e.(*ast.UnaryExpr).X
-		} else if _, ok := e.(*ast.ParenExpr); ok {
-			e = e.(*ast.ParenExpr).X
-		} else if _, ok := e.(*ast.SliceExpr); ok {
-			e = e.(*ast.SliceExpr).X
-		} else if _, ok := e.(*ast.IndexListExpr); ok {
-			e = e.(*ast.IndexListExpr).X
-		} else if _, ok := e.(*ast.StarExpr); ok {
-			e = e.(*ast.StarExpr).X
-		} else if _, ok := e.(*ast.TypeAssertExpr); ok {
-			e = e.(*ast.TypeAssertExpr).X
-		} else if _, ok := e.(*ast.CompositeLit); ok {
-			// TODO dummy implementation
-			if len(e.(*ast.CompositeLit).Elts) == 0 {
-				e = e.(*ast.CompositeLit).Type
-			} else {
-				e = e.(*ast.CompositeLit).Elts[0]
-			}
-		} else if _, ok := e.(*ast.KeyValueExpr); ok {
-			e = e.(*ast.KeyValueExpr).Value
-		} else {
-			// TODO this is uncaught expression
-			panic("uncaught expression")
-		}
-	}
-	if len(l) < 2 {
-		panic("selector list should have at least 2 elems")
-	}
-	// caller or receiver is always
-	// at position 1, function is at 0
-	return l[1]
-}
-
 func printPackageInfo(pkg *types.Package) {
 	fmt.Printf("Package  %q\n", pkg.Path())
 	fmt.Printf("Name:    %s\n", pkg.Name())
@@ -98,42 +46,8 @@ func printPackageInfo(pkg *types.Package) {
 func inspectFuncs(file *ast.File, fset *token.FileSet, info *types.Info, f *os.File) {
 	ast.Inspect(file, func(n ast.Node) bool {
 		if funDeclNode, ok := n.(*ast.FuncDecl); ok {
-			f.WriteString("FuncDecl:" + file.Name.Name + "." + funDeclNode.Name.String())
+			f.WriteString("FuncDecl:" + fset.Position(funDeclNode.Pos()).String() + file.Name.Name + "." + funDeclNode.Name.String())
 			f.WriteString("\n")
-			//printPackageInfo(pkg)
-			funType := info.Defs[funDeclNode.Name].Type()
-			var fTypeStr string
-			if funType != nil {
-				fTypeStr = funType.String()
-			}
-			f.WriteString("FuncDecl " +
-				fset.Position(funDeclNode.Pos()).String() + " " + funDeclNode.Name.String() + " " + fTypeStr)
-			f.WriteString("\n")
-		}
-		if callExpr, ok := n.(*ast.CallExpr); ok {
-			if id, ok := callExpr.Fun.(*ast.Ident); ok {
-				f.WriteString("CallExpr:" + file.Name.Name + ":" + id.Name)
-				f.WriteString("\n")
-				//printPackageInfo(pkg)
-				if info.Uses[id] != nil {
-					f.WriteString("CallExpr " +
-						fset.Position(id.Pos()).String() + " " + id.Name + " " + info.Uses[id].String())
-				}
-			}
-			if sel, ok := callExpr.Fun.(*ast.SelectorExpr); ok {
-				obj := info.Selections[sel]
-
-				if sel.Sel != nil && info.Uses[sel.Sel] != nil {
-					f.WriteString("Qualified " + info.Uses[sel.Sel].Name() + " " + info.Uses[sel.Sel].Type().String())
-					f.WriteString("\n")
-				}
-				if obj != nil {
-					f.WriteString("CallExpr " + fset.Position(obj.Obj().Pos()).String() + " " + obj.Obj().Name() +
-						" uses sel " + //+ info.Uses[GetMostInnerAstIdent(sel)].Type().String() +
-						" " + obj.Obj().Type().String())
-					f.WriteString("\n")
-				}
-			}
 		}
 		return true
 	})
@@ -157,32 +71,15 @@ func analyzePackage(filePaths []string, f *os.File) {
 		files = append(files, file)
 	}
 
-	srcPath := filepath.Dir(filePaths[0])
-	conf := types.Config{Importer: importer.Default()}
-	pkg, err := conf.Check(srcPath, fset, files, info)
-	if err != nil {
-		f.WriteString(err.Error())
-		f.WriteString("\n")
-	}
-	_ = pkg
 	for _, file := range files {
 		inspectFuncs(file, fset, info, f)
 	}
 }
 
-func main() {
+func compile(args []string, f *os.File) {
 	prog := `package main
 import "fmt"
 func main() {fmt.Println("hello")}`
-	_ = prog
-	f, _ := os.OpenFile("args", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	args := os.Args[1:]
-	cmdName := GetCommandName(args)
-	if cmdName != "compile" {
-		executePass(args[0:])
-		return
-	}
-
 	argsLen := len(args)
 	var destPath string
 	for i, a := range args {
@@ -226,4 +123,20 @@ func main() {fmt.Println("hello")}`
 		}
 	}
 	executePass(args[0:])
+
+}
+
+func main() {
+	prog := `package main
+import "fmt"
+func main() {fmt.Println("hello")}`
+	_ = prog
+	f, _ := os.OpenFile("args", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	args := os.Args[1:]
+	cmdName := GetCommandName(args)
+	if cmdName != "compile" {
+		executePass(args[0:])
+		return
+	}
+	compile(args, f)
 }
